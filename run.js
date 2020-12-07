fea4656db579c2898d0de25d025a2dd87e22ed2b
James Turle <james@foyerlive.com>
2020-12-07 11:45:36 -0500

    want that final slash please

--- run.js
+++ run.js
@@ -1,72 +1,79 @@
 #!/usr/bin/env node
 
-const builder = require('xmlbuilder');
-const { readdir, writeFile, lstat } = require('fs');
-const { promisify } = require('util');
+const builder = require("xmlbuilder");
+const { readdir, writeFile, lstat } = require("fs");
+const { promisify } = require("util");
 const args = process.argv;
 
-const preFormattedBaseUrl = args.find((arg) => arg.startsWith('--base-url='));
+const preFormattedBaseUrl = args.find((arg) => arg.startsWith("--base-url="));
 let baseUrl;
 
 if (preFormattedBaseUrl) {
-  baseUrl  = preFormattedBaseUrl.split('=')[1];
+  baseUrl = preFormattedBaseUrl.split("=")[1];
 } else {
-  throw (new Error('Missing valid --base-url command line argument'));
+  throw new Error("Missing valid --base-url command line argument");
 }
 
 if (baseUrl.length === 0) {
-  throw (new Error('Pass a valid url to --base-url'));
+  throw new Error("Pass a valid url to --base-url");
 }
 
-const asyncReaddir = promisify(readdir), asyncWriteFile = promisify(writeFile), asyncLStat = promisify(lstat);
+const asyncReaddir = promisify(readdir),
+  asyncWriteFile = promisify(writeFile),
+  asyncLStat = promisify(lstat);
 
 const asyncForEach = async (array, callback) => {
-    for (let index = 0; index < array.length; index++) {
-        await callback(array[index], index, array);
-    }
-}
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+};
 
 let allRoots = [];
 
 const readSite = async (dir) => {
-    const directory = await asyncReaddir(dir, { withFileTypes: true });
+  const directory = await asyncReaddir(dir, { withFileTypes: true });
 
-    await asyncForEach(directory, async fileOrDirectory => {
-        const root = `${dir}/${fileOrDirectory}`;
-        const stat = await asyncLStat(root);
+  await asyncForEach(directory, async (fileOrDirectory) => {
+    const root = `${dir}/${fileOrDirectory}`;
+    const stat = await asyncLStat(root);
 
-        if (stat.isDirectory()) {
-            await readSite(root);
-        } else if (fileOrDirectory === 'index.html') {
-            allRoots = [...allRoots, root];
-        }
-    });
-}
+    if (stat.isDirectory()) {
+      await readSite(root);
+    } else if (fileOrDirectory === "index.html") {
+      allRoots = [...allRoots, root];
+    }
+  });
+};
 
 const blackList = [];
 
 (async () => {
-    await readSite('build');
+  await readSite("build");
 
-    const siteUrls = allRoots.map(root => root.replace('build/', baseUrl).replace('/index.html', ''));
+  const siteUrls = allRoots.map((root) =>
+    root.replace("build/", baseUrl).replace("/index.html", "/")
+  );
 
-    const urlset = builder.create('urlset', { encoding: 'UTF-8', version: '1.0' });
+  const urlset = builder.create("urlset", {
+    encoding: "UTF-8",
+    version: "1.0",
+  });
 
-    urlset.attribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+  urlset.attribute("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9");
 
-    siteUrls
-        .filter(url => !blackList.includes(url))
-        .sort((a, b) => a.length - b.length)
-        .forEach(url => {
-            const u = urlset.ele('url');
+  siteUrls
+    .filter((url) => !blackList.includes(url))
+    .sort((a, b) => a.length - b.length)
+    .forEach((url) => {
+      const u = urlset.ele("url");
 
-            u.ele('loc', url);
-            u.ele('priority', 0.5);
-        });
+      u.ele("loc", url);
+      u.ele("priority", 0.5);
+    });
 
-    const sitemap = urlset.end({ pretty: true });
+  const sitemap = urlset.end({ pretty: true });
 
-    await asyncWriteFile('build/sitemap.xml', sitemap);
+  await asyncWriteFile("build/sitemap.xml", sitemap);
 
-    console.log('🙂 Successfully built sitemap.xml in build directory');
+  console.log("🙂 Successfully built sitemap.xml in build directory");
 })();
